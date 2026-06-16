c8ed54a4a2f4d90f655513593e1f2f21524c2247
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-06-16 18:01:21 +0530

    addStudent
    save through api into data base

--- lms-fronted/src/components/Navigation/ListStudent.jsx
+++ lms-fronted/src/components/Navigation/ListStudent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { listStudent } from '../services/StudentService'
+import { listStudent } from '../../services/StudentService'
 
 const ListStudent = () => {
 
