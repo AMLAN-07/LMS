f6adba131effe619e21c3e4c4e15b56710847fe6
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-06-25 23:42:38 +0530

    Backend book Curd

---
+++ LibraryManagement/src/main/java/com/library/backends/entity/Book.java
@@ -0,0 +1,26 @@
+package com.library.backends.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    private String title;
+    private String author;
+    @Column(name="book_copy")
+    private int bookcopy;
+
+}
