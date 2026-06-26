037d5826f8d948d73cb0531952aaf834a78ec1c8
Hira lal Kumar <hk2493200@gmail.com>
2026-06-26 17:37:39 +0530

    Update StudentDto.java

--- LibraryManagement/src/main/java/com/library/backends/dto/StudentDto.java
+++ LibraryManagement/src/main/java/com/library/backends/dto/StudentDto.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 
 public class StudentDto {
+    //student data member 
     private Long id;
     private String firstName;
     private String lastName;
