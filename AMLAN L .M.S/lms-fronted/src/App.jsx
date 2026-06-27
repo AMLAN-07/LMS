69a278c39a4aba5a1827fe6fdd2ebb1a4dc62f81
Tapas999 <tapaskumarjena999@gmail.com>
2026-06-27 09:03:30 +0530

    BOOK SECTION
    COMPLETED

---
+++ AMLAN L .M.S/lms-fronted/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import StudentInfo from './components/StudentInfo'
+import AddStudent from './components/Navigation/AddStudent'
+import Dashboard from './components/Dashboard/Dashboard'
+import BookSection from './components/Books/BookSection'
+
+const App = () => {
+
+  return (
+  <>
+    <BrowserRouter>
+     <Header/>
+      <Routes>
+        {/* //http://localhost:3000 */}
+        <Route path='/' element={<Dashboard/>
+
+
+      }></Route>
+        {/* //http://localhost:3000/students */}
+        <Route path='/students' element={<StudentInfo/>}></Route>
+        {/* //http://localhost:3000/books */}
+        <Route path='/books' element={<BookSection/>}></Route>
+        {/* //http://localhost:3000/add-student */}
+        <Route path='/add-student' element={<AddStudent/>}></Route>
+        {/* //http://localhost:3000/edit-student/#id */}
+        <Route path='/edit-student/:id' element={<AddStudent/>}></Route>
+      </Routes>
+
+    </BrowserRouter>
+  </>
+  )
+}
+
+export default App
